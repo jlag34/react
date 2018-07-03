37056009b33d1eb5650d06e082309c5641e8fe60
Joe Lagasse <joe@servant.co>
2018-07-03 19:22:22 -0400

    Upgrade to react ^16, webpack ^4, babel ^7. Included redux

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,26 @@ var webpack = require('webpack');
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const VENDOR_LIBS = [ 'react', 'react-dom' ];
+const VENDOR_LIBS = [
+  'axios', 'react', 'redux', 'react-dom', 'react-redux',
+  'react-router', 'redux-promise'
+];
 
 module.exports = {
   entry: {
     bundle: './src/index.js',
-    vendor: VENDOR_LIBS
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
       {
-        use: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
       },
       {
         use: ['style-loader', 'css-loader', "sass-loader"],
@@ -37,17 +40,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor', 'manifest']
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
-  devServer: {
-    historyApiFallback: true
-  }
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ]
 };
